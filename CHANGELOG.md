1391fb587354571fb43d7dde58cb3d09471d8564
arakilian0 <arakilian0@gmail.com>
2020-06-16 15:56:55 -0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 0.0.0
+- Initial commit
+- Project Setup (boilerplate code)
+- MIT License
---
+++ src/cola.js
@@ -0,0 +1,7 @@
+/*!
+ * cola.js v0.0.0 
+ * (c) 2020 Michael Arakilian
+ * Released under the MIT License.
+ */
+
+ console.log('hello world')
